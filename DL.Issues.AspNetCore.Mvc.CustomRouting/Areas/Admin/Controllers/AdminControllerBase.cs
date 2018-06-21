fe950122429fe2f8beb81ac7670976c9cad09058
David Liang <david.liang@outlook.com>
2018-06-21 15:44:01 -0700

    Add comment next to the admin area attribute.

--- DL.Issues.AspNetCore.Mvc.CustomRouting/Areas/Admin/Controllers/AdminControllerBase.cs
+++ DL.Issues.AspNetCore.Mvc.CustomRouting/Areas/Admin/Controllers/AdminControllerBase.cs
@@ -2,6 +2,8 @@
 
 namespace DL.Issues.AspNetCore.Mvc.CustomRouting.Areas.Admin.Controllers
 {
+    // If you comment the [Area] attribute out, everything works.
+    // But if you have it on, previous routings with area="" will not work!
     [Area("admin")]
     public abstract class AdminControllerBase : Controller
     {
