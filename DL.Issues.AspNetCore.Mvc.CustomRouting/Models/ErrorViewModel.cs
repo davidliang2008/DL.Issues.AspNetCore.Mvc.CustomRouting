e38943a57fc7c4b2b5b04de49596c4547332916b
David Liang <david.liang@outlook.com>
2018-06-21 14:26:15 -0700

    Add project files.

---
+++ DL.Issues.AspNetCore.Mvc.CustomRouting/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DL.Issues.AspNetCore.Mvc.CustomRouting.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
